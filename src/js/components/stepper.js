7f79b37c029995e2dc380329a66267e95c088e95
= <=>
2026-05-21 20:01:12 +0300

    create: logic for stepper

---
+++ src/js/components/stepper.js
@@ -0,0 +1,51 @@
+const steppers = document.querySelectorAll('.stepper');
+
+if (steppers) {
+  steppers.forEach((stepper) => {
+    const input = stepper.querySelector('.stepper__input');
+    const minus = stepper.querySelector('.stepper__minus');
+    const plus = stepper.querySelector('.stepper__plus');
+
+    const updateButtons = (value) => {
+      if (value <= 1) {
+        minus.classList.add('stepper__disabled');
+      } else {
+        minus.classList.remove('stepper__disabled');
+      }
+
+      if (value >= 99) {
+        plus.classList.add('stepper__disabled');
+      } else {
+        plus.classList.remove('stepper__disabled');
+      }
+    };
+
+    const updateValue = (newValue) => {
+      let num = parseInt(newValue);
+
+      if (isNaN(num)) num = 1;
+      if (num < 1) num = 1;
+      if (num > 99) num = 99;
+
+      input.value = num;
+      updateButtons(num);
+      return num;
+    };
+
+    input.addEventListener('input', () => {
+      updateValue(input.value);
+    });
+
+    minus.addEventListener('click', () => {
+      let currentValue = parseInt(input.value);
+      updateValue(currentValue - 1);
+    });
+
+    plus.addEventListener('click', () => {
+      let currentValue = parseInt(input.value);
+      updateValue(currentValue + 1);
+    });
+
+    updateValue(input.value);
+  });
+}
